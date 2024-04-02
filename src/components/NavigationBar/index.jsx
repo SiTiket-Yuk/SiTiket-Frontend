0744fd5539ea06cbf6da661562e758a1452ce1d1
Arifki Ilham <arifkiilham11@gmail.com>
2024-04-03 01:11:23 +0700

    added Bantuan

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import LogoLight from "../../../public/ImgLogo/LogoLight.png";
+import LogoLight from "../../../public/logo/LogoLight.png";
 
 const NavigationBar = () => {
 	return (
 		<nav className="bg-blue">
 			<div
-				className="flex flex-wrap md:flex-row flex-col lg:px-52 md:px-32 
-				sm:px-0 py-5 justify-between items-center"
+				className="flex flex-wrap md:flex-row flex-col lg:mx-52 md:mx-20
+				py-5 justify-between items-center"
 			>
 				<div className="flex md:flex-row md:gap-12 gap-1 flex-col items-center">
 					<Link href="/" className="flex flex-row items-center">
@@ -17,7 +17,7 @@ const NavigationBar = () => {
 							alt="SiTiket Logo"
 						/>
 						<div className="flex flex-row pl-2 text-white text-3xl">
-							si<div class="font-bold">tiket</div>
+							si<div className="font-bold">tiket</div>
 						</div>
 					</Link>
 					<Link
@@ -32,11 +32,17 @@ const NavigationBar = () => {
 					>
 						Tentang Kami
 					</Link>
+					<Link
+						href="#"
+						className="block rounded text-xl text-navbar hover:text-white"
+					>
+						Bantuan
+					</Link>
 				</div>
 				<Link href="#">
 					<div
 						className="block rounded-full text-white font-bold border-white 
-						border-2 p-3 px-5 over:bg-blue-300 hover:border-blue-300 "
+						border-2 p-3 px-5 over:bg-blue-300 hover:border-blue-100 hover:text-blue-100"
 					>
 						Buat Akun
 					</div>
