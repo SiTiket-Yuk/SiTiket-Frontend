4ad006da421b79bce0cd49e2a0ad807be4347436
Arifki Ilham <arifkiilham11@gmail.com>
2024-04-03 01:09:33 +0700

    created event card

---
+++ src/components/EventCard/index.jsx
@@ -0,0 +1,39 @@
+import {
+	Avatar,
+	Card,
+	CardBody,
+	CardFooter,
+	Divider,
+	Image,
+} from "@nextui-org/react";
+
+const EventCard = ({
+	eventImg,
+	eventName,
+	date,
+	cost,
+	organizerLogo,
+	organizerName,
+}) => {
+	const formattedCost = cost.toLocaleString("id-ID");
+
+	return (
+		<Card className="max-w-[450px] min-w-[200px]">
+			<CardBody className="p-5">
+				<Image src={eventImg.src} alt={eventName} />
+				<div>
+					<h3 className="py-5 font-medium text-xl">{eventName}</h3>
+					<h4 className="pb-3 text-base text-[#929292]">{date}</h4>
+					<h3 className="font-bold text-xl">Rp. {formattedCost}</h3>
+				</div>
+			</CardBody>
+			<Divider />
+			<CardFooter className="flex gap-4">
+				<Avatar radius="full" size="md" src={organizerLogo.src} />
+				<h4>{organizerName}</h4>
+			</CardFooter>
+		</Card>
+	);
+};
+
+export default EventCard;
