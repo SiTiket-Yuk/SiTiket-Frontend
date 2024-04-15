fea96358db81100adbaa4f79db166b46b2eb71af
Arifki Ilham <arifkiilham11@gmail.com>
2024-04-15 22:13:46 +0700

    fix Link href

--- src/components/CategoryBar/index.jsx
+++ src/components/CategoryBar/index.jsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
 import React from "react";
-import { Link } from "next/link";
 const EventCategory = ({ categories }) => {
 	return (
 		<div className="flex flex-nowrap mt-8 text-base category ">
 			<p className="font-bold pr-4">Kategori: </p>
 			<div className=" text-sky-500 underline">
 				{categories.map((category, index) => (
-					<Link key={index} className="mr-3" href={category.toLowerCase()}>
+					<Link
+						key={index}
+						className="mr-3"
+						/*{benerin hrefnya kalo discovery udah bisa}*/
+						href={`/${category.toLowerCase()}`}
+					>
 						{category}
 					</Link>
 				))}
