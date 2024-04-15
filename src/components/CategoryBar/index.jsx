e3189ab0e1139b2e40ffd558eb6e638019a1133b
Arifki Ilham <arifkiilham11@gmail.com>
2024-04-15 22:04:37 +0700

    change to Link

--- src/components/CategoryBar/index.jsx
+++ src/components/CategoryBar/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "@nextui-org/react";
 const EventCategory = ({ categories }) => {
 	return (
 		<div className="flex flex-nowrap mt-8 text-base category ">
 			<p className="font-bold pr-4">Kategori: </p>
 			<div className=" text-sky-500 underline">
 				{categories.map((category, index) => (
-					<a key={index} className="mr-3" href={category.toLowerCase()}>
+					<Link key={index} className="mr-3" href={category.toLowerCase()}>
 						{category}
-					</a>
+					</Link>
 				))}
 			</div>
 		</div>
