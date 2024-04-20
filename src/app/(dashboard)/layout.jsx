7a195d0e1fb18e886b0b48df652f848e25cdb936
Arifki Ilham <arifkiilham11@gmail.com>
2024-04-20 23:37:18 +0700

    refactor code done

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,13 +1,21 @@
 import { DM_Sans } from "next/font/google";
 import "../globals.css";
-import NavigationBar from "@/components/NavigationBar";
+import SideNavigationBar from "@/components/SideNavigationBar";
+import { Providers } from "../providers";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 const RootLayout = ({ children }) => {
 	return (
 		<html lang="en">
-			<body className={dmSans.className}>{children} </body>
+			<body className={dmSans.className}>
+				<Providers>
+					<div className="flex flex-row">
+						<SideNavigationBar />
+						{children}
+					</div>
+				</Providers>
+			</body>
 		</html>
 	);
 };
